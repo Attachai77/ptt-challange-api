e2e2bf3054c94d44e2f70ddfce01e98b5bd058a0
Attachai (Wick) <attachai_sao@true-e-logistics.com>
2021-11-05 14:57:45 +0700

    setup typescript & add pyramid function

---
+++ src/pyramid.ts
@@ -0,0 +1,17 @@
+const pyramid = (number: number) => {
+  for (let increment = 1; increment <= number; increment++) {
+    let row = ''
+
+    for (let j = 1; j <= number - increment; j++) {
+      row += '  ';
+    }
+
+    for (let column = 1; column <= increment; column++) {
+      row += `  ${increment} `
+    }
+    console.log(row);
+  }
+}
+
+
+pyramid(9)
